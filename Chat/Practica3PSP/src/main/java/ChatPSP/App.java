01ca9e61fb2f52f741b599d25d667243ac37e46d
ElManu3le <msancho620@yahoo.es>
2021-02-12 17:33:18 +0100

    SrvidorChatInicio

--- Chat/Practica3PSP/src/main/java/ChatPSP/App.java
+++ Chat/Practica3PSP/src/main/java/ChatPSP/App.java
@@ -1,13 +1,27 @@
 package ChatPSP;
 
-/**
- * Hello world!
- *
- */
+import java.net.*;
+import java.io.*;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    static final String IP = "localhost";
+    static final int PUERTO = 4444;
+  
+    public static void main(String[] args) throws IOException {
+      // Obtenemos conexion e inicializamos
+      Socket socketTcp = new Socket(IP, PUERTO);
+  
+      // Obtenemos los canales de entrada de datos y de salida
+      DataInputStream entrada = new DataInputStream(socketTcp.getInputStream());
+      DataOutputStream salida = new DataOutputStream(socketTcp.getOutputStream());
+  
+      // Enviamos un mensaje y esperamos la respuesta del servidor
+      salida.writeUTF("patata");
+      String mensajeDelServidor = entrada.readUTF();
+      System.out.println("Recibido mensaje del servidor: " + mensajeDelServidor);
+  
+      // Cerramos la conexión
+      socketTcp.close();
+  
     }
 }
